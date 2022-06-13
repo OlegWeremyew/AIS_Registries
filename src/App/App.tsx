1c116a990227ede938dec478a163cc48de67e509
olegweremyew <olegweremey1994@mail.ru>
2022-06-13 17:22:02 +0300

    Initial commit

---
+++ src/App/App.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+import './App.css';
+import { ReturnComponentType } from '../types';
+
+const App = (): ReturnComponentType => <div className="App">gd</div>;
+
+export default App;
